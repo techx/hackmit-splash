855deaf605ab48a9f7f7f61ceadc24e40dc2d4a4
Noah Moroze <noahmoroze@gmail.com>
2017-06-11 19:49:51 -0400

    Adjust spinner to reflect days remaining

--- js/app.js
+++ js/app.js
@@ -62,18 +62,25 @@
 	// COUNTDOWN
 	////////////////////
 
-	var launchDate = new Date('Sep 16 2017');
-	var remainingMessage = " DAYS REMAINING";
-	var launchMessage = "";
-
-	/* Handles timezone conversions */
-	function updateCountdown(countdownElement) {
-		var localDate = new Date();
+	/* Handle timezone conversions */
+    function daysToHack() {
+	    var launchDate = new Date('Sep 16 2017');
+        var localDate = new Date();
 		var utc = localDate.getTime() + (localDate.getTimezoneOffset() * 60000);
 		var edtNow = new Date(utc + (3600000*(-4)));
 
 		var millisecondsPerDay = 24 * 60 * 60 * 1000;
 		var numberDays = Math.round((launchDate - edtNow) / millisecondsPerDay);
+
+        return numberDays;
+    }
+
+    /* Update DOM */ 	
+	function updateCountdown(countdownElement) {
+        var remainingMessage = " DAYS REMAINING";
+        var launchMessage = "";
+        
+        var numberDays = daysToHack(); 
 
 		if (numberDays > 0) {
 			countdownElement.innerHTML = numberDays + remainingMessage;
@@ -83,8 +90,33 @@
 		}
 	}
 
+    function updateSpinner(spinnerElement) {
+        var MAX_DAYS = 100; 
+
+        // must wait for svg to load before modifying
+        spinnerElement.addEventListener("load", function() {
+            // inspired by https://codepen.io/JMChristensen/pen/Ablch?editors=1111
+            var spinnerDoc = spinnerElement.contentDocument;
+            var timer = spinnerDoc.getElementById("timer");
+            console.log(spinnerDoc); 
+            console.log(timer);
+            var r = timer.getAttribute("r");
+            var c = Math.PI * (r * 2); 
+            
+            var days = daysToHack(); 
+
+            // clamp # days remaining 
+            if (days < 0) { days = 0;}
+            if (days > MAX_DAYS) { days = MAX_DAYS;}
+            
+            var dashOffset = ((MAX_DAYS-days)/MAX_DAYS)*c;
+            timer.style.strokeDashoffset = dashOffset;  
+        }); 
+    }   
+
 	Array
 		.from(document.getElementsByClassName('days'))
 		.forEach(updateCountdown);
 
+    updateSpinner(document.getElementById("spinner"));
 })();
