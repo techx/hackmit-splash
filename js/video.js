bf7e77a7a821ebfc91ba1530bcc3f86309d6ce3e
Ajay Jain <ajayjain@mit.edu>
2017-07-01 03:07:16 -0700

    Switch from jQuery to Zepto, improve video animation

--- js/video.js
+++ js/video.js
@@ -2,26 +2,34 @@
 // VIDEO THINGS
 ////////////////////
 
-
 $(document).ready(function() {
 
 	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
+    $splash = $("#splash");
+    $video = $("#fs-video");
+    $spinner = $("#spinner");
+
 	// first visit and not mobile
 	if (!localStorage.noFirstVisit && width >= 768) {
-		localStorage.noFirstVisit = "1";
-		$("body").append('<div id="fs-container"><video muted autoplay id="fs-video" style="display:none"><source src="/assets/videos/splash-intro-compressed.mp4" type="video/mp4"></video></div>');
-		$("#fs-video").css("display", "block");
-		$("#splash").css("display", "none");
-		$("#splash").css("opacity", "0");
+        localStorage.noFirstVisit = "1";
 
-		$("#fs-video").on("ended", function() {
-			$("#fs-video").fadeOut(500);
-			$("#splash").css("display", "inline");
-			setTimeout(function(){
-				$("#splash").animate({"opacity":"+=1"}, 1000);
-			}, 500);
+        $video.css("display", "block");
+
+        document.getElementById('fs-video').play();
+
+		$video.on("ended", function() {
+			$splash.css("visibility", "visible");
+            $splash.animate({"opacity": 1}, 1000, "ease-in");
+
+            $video.animate({"opacity": 0}, 500, "ease-out", function () {
+                $("#fs-container").remove();
+            });
 		});
+	} else {
+        $("#fs-container").remove();
+        $splash.css("visibility", "visible");
+        $splash.css("opacity", 1)
 	}
 
-})
+});
