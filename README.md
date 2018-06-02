9d55a19587ce0a7a6bf0750cd28b0357954bd6e6
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-06-02 15:24:36 -0700

    Added Gulp build process

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # HackMIT 2018 Splash Page
 Splash page for HackMIT 2018!
+
 ## Development
+
 ## Design
  - [Branding](https://drive.google.com/drive/folders/1JL5YErAqbnb6cnxHNmOzOGL7OuBwepyS)
  - [Desktop View](https://xd.adobe.com/view/8ff7e09f-0072-427e-459a-3ec8a18130ff-f9c6/)
  - [Mobile View]()
- ## TODO
+
+## TODO
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+  ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2018-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/techx/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  ''
+].join('');
+
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function() {
+  return gulp.src('scss/styles.scss')
+    .pipe(sass())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function() {
+  return gulp.src('css/styles.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Default task
+gulp.task('default', ['sass', 'minify-css']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'sass', 'minify-css'], function() {
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('css/*.css', ['minify-css']);
+  // Reloads the browser whenever HTML files change
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('js/*.js', browserSync.reload);
+});
