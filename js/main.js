4e79fa7b03c70532184afab28f51619da74bb9f4
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-06-02 22:57:50 -0700

    Continued parallax; added initial offsets and background colors

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ window.addEventListener('scroll', function(event) {
     var layers = document.getElementsByClassName('layer');
     for(var i = 0; i<layers.length; i++) {
         var speed = layers[i].getAttribute('data-speed');
-        var yPos = -top*speed;
+        var offset = parseInt(layers[i].getAttribute('data-offset'));
+        var yPos = -(top*speed)+offset;
         layers[i].setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
     }
 });
