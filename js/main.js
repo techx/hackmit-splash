80a9be66927fcb7840dd1bf9ee34b53b319d200a
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-06-02 22:40:20 -0700

    Added 3 parallax layers

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ window.addEventListener('scroll', function(event) {
     for(var i = 0; i<layers.length; i++) {
         var speed = layers[i].getAttribute('data-speed');
         var yPos = -top*speed;
-        layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+        layers[i].setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
     }
 });
