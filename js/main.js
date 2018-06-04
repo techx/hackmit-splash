994a01fadf6c0b927688e0ab822821159f4743fc
jynnie <jynnie@mit.edu>
2018-06-03 23:10:21 -0400

    pure css parallax scrolling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-window.addEventListener('scroll', function(event) {
-    var top = this.pageYOffset;
-    var layers = document.getElementsByClassName('layer');
-    for(var i = 0; i<layers.length; i++) {
-        var speed = layers[i].getAttribute('data-speed');
-        var offset = parseInt(layers[i].getAttribute('data-offset'));
-        var yPos = -(top*speed)+offset;
-        layers[i].setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
-    }
-});
+// window.addEventListener('scroll', function(event) {
+//     var top = this.pageYOffset;
+//     var layers = document.getElementsByClassName('layer');
+//     for(var i = 0; i<layers.length; i++) {
+//         var speed = layers[i].getAttribute('data-speed');
+//         var offset = parseInt(layers[i].getAttribute('data-offset'));
+//         var yPos = -(top*speed)+offset;
+//         layers[i].setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+//     }
+// });
