a6e82d01ef5e2ba713d4cd4653183196de991828
Rahul Yesantharao <ryesantharao@mbp-ryesantharao.ad.quora.com>
2018-06-14 13:42:05 -0700

    Updated fade behavior of expanded bubbles

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// window.addEventListener('scroll', function(event) {
-//     var top = this.pageYOffset;
-//     var layers = document.getElementsByClassName('layer');
-//     for(var i = 0; i<layers.length; i++) {
-//         var speed = layers[i].getAttribute('data-speed');
-//         var offset = parseInt(layers[i].getAttribute('data-offset'));
-//         var yPos = -(top*speed)+offset;
-//         layers[i].setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
-//     }
-// });
-
 window.onload = function() {
     var signs = document.getElementsByClassName('sign-link');
     console.log(signs);
@@ -33,22 +22,6 @@ window.onload = function() {
                 }
             });
         }());
-        // var sign = signs[i].getSVGDocument();
-        // sign.addEventListener('mouseover', function() {
-        //     var glow = this.querySelectorAll('.glow');
-        //     console.log(glow);
-        //     for (var j = 0; j < glow.length; j++) {
-        //         glow[j].classList.add('flash-animation');
-        //     }
-        // });
-
-        // sign.addEventListener('mouseleave', function() {
-        //     var glow = this.querySelectorAll('.glow');
-        //     for (var j = 0; j < glow.length; j++) {
-        //         glow[j].classList.remove('flash-animation');
-        //         glow[j].style.opacity = 1;
-        //     }
-        // });
     }
 
     var signNames = ['speakers', 'faq', 'sponsors', 'register'];
@@ -63,37 +36,20 @@ window.onload = function() {
                     signLink.addEventListener('click', function() {
                         var expandedSign = document.getElementById(name + '-expanded');
                         console.log("SHOW");
-                        // hide the signs
-                        var smallSigns = document.getElementsByClassName('sign-link');
-                        for(var j=0; j<smallSigns.length; j++) {
-                            smallSigns[j].classList.add('hide');
-                        }
-                        var blurb = document.getElementsByClassName('blurb')[0];
-                        blurb.classList.add('hide');
                         expandedSign.classList.add('show1');
-                        // expandedSign.classList.add('show2')
                         setTimeout(function() {expandedSign.classList.add('show2')}, 1);
                     });
                 }());
             }
         }());
     }
-    for(var i = 0; i<signNames.length; i++) {
+    for(var i=0; i<signNames.length; i++) {
         (function() {
-            var name = signNames[i];
-            var arrow = document.getElementById(name + '-arrow');
-            arrow.addEventListener('click', function() {
-                var expandedSign = document.getElementById(name + '-expanded');
-                console.log("HIDE");
-                // show the signs
-                var smallSigns = document.getElementsByClassName('sign-link');
-                for(var j=0; j<smallSigns.length; j++) {
-                    smallSigns[j].classList.remove('hide');
-                }
-                var blurb = document.getElementsByClassName('blurb')[0];
-                blurb.classList.remove('hide');
-                expandedSign.classList.remove('show2');
-                setTimeout(function() {expandedSign.classList.remove('show1');}, 500);
+            var expandedDiv = document.getElementById(signNames[i] + '-expanded');
+            expandedDiv.addEventListener('click', function(e) {
+                if(e.target.closest(".expanded-bubble-left") || e.target.closest(".expanded-bubble-right")) return;
+                expandedDiv.classList.remove('show2');
+                setTimeout(function() {expandedDiv.classList.remove('show1');}, 500);
             });
         }());
     }
