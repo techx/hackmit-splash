9f8f25190122cb01c68688ec0f45e32480f6aba5
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-06-17 03:00:07 -0700

    scroll fix potentially

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 window.onload = function() {
+    setTimeout(function(){
+		// Hide the address bar!
+		window.scrollTo(0, 1);
+	}, 0);
     ///////////////
     // Glowing Signs
     ///////////////
