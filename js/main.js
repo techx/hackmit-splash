d6206cd47d492471635fa98ad5c56156a121b19f
rk9109 <rk9109@gmail.com>
2018-08-28 18:07:07 -0400

    Initial speakers layout

--- js/main.js
+++ js/main.js
@@ -90,6 +90,37 @@ window.onload = function() {
 		});
 }
 
+///////////////
+// Slideshow
+///////////////
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
 /* LIT PUZZLE ENTRY */
 
 $('.bottom').click(function (e) {
