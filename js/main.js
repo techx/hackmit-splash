b8d025963115f4362f435f3e8322fbd795398746
rk9109 <rk9109@gmail.com>
2018-08-28 19:17:23 -0400

    Better resizing

--- js/main.js
+++ js/main.js
@@ -110,12 +110,11 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
